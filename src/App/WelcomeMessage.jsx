3b00470a1f509953a890e847525ec98c8eaea8a8
Mark Dawes <mrkdawes8@gmail.com>
2022-07-13 12:13:21 -0400

    add appbar and grid

--- src/App/WelcomeMessage.jsx
+++ src/App/WelcomeMessage.jsx
@@ -1,3 +1,3 @@
 export default function Welcome() {
-  return <h1>Welcome to CryptoDash</h1>;
+  return <h1>Welcome to CryptoData</h1>;
 }
--- src/App/index.jsx
+++ src/App/index.jsx
@@ -2,10 +2,12 @@ import './App.css';
 import WelcomeMessage from './WelcomeMessage'
 import styled, {css} from 'styled-components'
 import AppLayout from './AppLayout'
+import AppBar from './AppBar'
 
 function App() {
   return (
     <AppLayout>
+      <AppBar/>
       <WelcomeMessage/>
     </AppLayout>
   );
