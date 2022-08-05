75c995a2c0ed0364682265c5c5fbd20233e4edd7
Mark Dawes <mrkdawes8@gmail.com>
2022-08-05 18:55:59 -0400

    add coin grid

--- src/Settings/index.jsx
+++ src/Settings/index.jsx
@@ -1,10 +1,12 @@
 import WelcomeMessage from "./WelcomeMessage";
 import ConfirmButton from "./ConfirmButton";
 import Page from '../Shared/Page';
+import CoinGrid from './CoinGrid';
 
 export default function(){
   return <Page name="settings"> 
     <WelcomeMessage/> 
     <ConfirmButton/> 
+    <CoinGrid/>
   </Page>
 }
