dee8465b9832e706586d7602eaf8fb40bec30b4d
Mark Dawes <mrkdawes8@gmail.com>
2022-08-05 23:03:37 -0400

    finish add/remove coins

--- src/App/AppProvider.jsx
+++ src/App/AppProvider.jsx
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React from 'react'
 
 const cc = require('cryptocompare')
@@ -5,13 +6,19 @@ cc.setApiKey('427a751f660e56e70af19732b49a44c1e63935324d3f1a0e7350215a3c199009')
 
 export const AppContext = React.createContext();
 
+const MAX_FAVORITES = 10;
+
 export class AppProvider extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       page: 'dashboard',
+      favorites: ['BTC', 'ETH', 'XMR', 'LTC'],
       ...this.savedSettings(),
       setPage: this.setPage,
+      addCoin: this.addCoin,
+      removeCoin: this.removeCoin,
+      isInFavorites: this.isInFavorites,
       confirmFavorites: this.confirmFavorites
     }
   }
@@ -25,22 +32,38 @@ export class AppProvider extends React.Component {
     this.setState({coinList});
   }
 
+  addCoin = key => {
+    let favorites = [...this.state.favorites];
+    if(favorites.length < MAX_FAVORITES){
+      favorites.push(key);
+      this.setState({favorites});
+    }
+  }
+
+  removeCoin = key => {
+    let favorites = [...this.state.favorites];
+    this.setState({favorites: _.pull(favorites, key)})
+  }
+
+  isInFavorites = key => _.includes(this.state.favorites, key)
+
   confirmFavorites = () => {
     this.setState({
       firstVisit: false,
       page: 'dashboard'
     });
     localStorage.setItem('cryptoData', JSON.stringify({
-      test: 'hello'
+      favorites: this.state.favorites
     }))
   }
 
   savedSettings(){
-    let cryptoDataData = JSON.parse(localStorage.getItem('cryptoData'));
-    if(!cryptoDataData){
+    let cryptoDashData = JSON.parse(localStorage.getItem('cryptoData'));
+    if(!cryptoDashData){
       return {page: 'settings', firstVisit: true}
     }
-    return {};
+    let {favorites} = cryptoDashData;
+    return {favorites};
   }
 
   setPage = page => this.setState({page})
