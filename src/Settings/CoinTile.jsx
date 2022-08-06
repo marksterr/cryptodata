70c5a4377018f393d243b08d5615dd1415921aaa
Mark Dawes <mrkdawes8@gmail.com>
2022-08-05 22:05:21 -0400

    add cointile

---
+++ src/Settings/CoinTile.jsx
@@ -0,0 +1,20 @@
+import {AppContext} from '../App/AppProvider'
+import { SelectableTile } from '../Shared/Tile'
+import CoinHeaderGrid from './CoinHeaderGrid'
+import CoinImage from '../Shared/CoinImage'
+
+export default function({coinKey}){
+
+  const TileClass = SelectableTile;
+
+  return <AppContext.Consumer>
+    {({coinList}) => {
+      let coin = coinList[coinKey];
+      const TileClass = SelectableTile;
+      return <TileClass>
+        <CoinHeaderGrid name={coin.CoinName} symbol={coin.Symbol}/>
+        <CoinImage coin={coin}/>
+      </TileClass>
+    }}
+  </AppContext.Consumer>
+}
