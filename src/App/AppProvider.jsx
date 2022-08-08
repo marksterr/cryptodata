d2090bace1efa92b2432c57707a916368249471a
Mark Dawes <mrkdawes8@gmail.com>
2022-08-07 21:01:14 -0400

    add setcurrentfavorite

--- src/App/AppProvider.jsx
+++ src/App/AppProvider.jsx
@@ -20,6 +20,7 @@ export class AppProvider extends React.Component {
       removeCoin: this.removeCoin,
       isInFavorites: this.isInFavorites,
       confirmFavorites: this.confirmFavorites,
+      setCurrentFavorite: this.setCurrentFavorite,
       setFilteredCoins: this.setFilteredCoins
     }
   }
@@ -69,14 +70,27 @@ export class AppProvider extends React.Component {
   isInFavorites = key => _.includes(this.state.favorites, key)
 
   confirmFavorites = () => {
+    let currentFavorite = this.state.favorites[0];
     this.setState({
       firstVisit: false,
-      page: 'dashboard'
+      page: 'dashboard',
+      currentFavorite,
     }, () => {
       this.fetchPrices();
     });
     localStorage.setItem('cryptoData', JSON.stringify({
-      favorites: this.state.favorites
+      favorites: this.state.favorites,
+      currentFavorite
+    }))
+  }
+
+  setCurrentFavorite = (sym) => {
+    this.setState({
+      currentFavorite: sym
+    });
+    localStorage.setItem('cryptoDash', JSON.stringify({
+      ...JSON.parse(localStorage.getItem('cryptoDash')),
+      currentFavorite: sym
     }))
   }
 
@@ -85,8 +99,8 @@ export class AppProvider extends React.Component {
     if(!cryptoDashData){
       return {page: 'settings', firstVisit: true}
     }
-    let {favorites} = cryptoDashData;
-    return {favorites};
+    let {favorites, currentFavorite} = cryptoDashData;
+    return {favorites, currentFavorite};
   }
 
   setPage = page => this.setState({page})
--- src/Dashboard/PriceTile.jsx
+++ src/Dashboard/PriceTile.jsx
@@ -1,7 +1,8 @@
 import styled, {css} from 'styled-components'
 import { SelectableTile } from '../Shared/Tile'
-import { fontSize3, fontSizeBig } from '../Shared/Styles';
+import { fontSize3, fontSizeBig, greenBoxShadow } from '../Shared/Styles';
 import { CoinHeaderGridStyled } from '../Settings/CoinHeaderGrid';
+import { AppContext } from '../App/AppProvider';
 
 const JustifyRight = styled.div`
   justify-self: right;
@@ -33,6 +34,11 @@ const PriceTileStyled = styled(SelectableTile)`
     grid-template-columns: repeat(3, 1fr);
     justify-items: right;
   `};
+
+  ${props => props.currentFavorite && css`
+    ${greenBoxShadow};
+    pointer-events: none;
+  `};
 `
 
 function ChangePercent({data}){
@@ -45,9 +51,9 @@ function ChangePercent({data}){
   );
 }
 
-function PriceTile({sym, data}){
+function PriceTile({sym, data, currentFavorite, setCurrentFavorite}){
   return (
-    <PriceTileStyled>
+    <PriceTileStyled onClick={setCurrentFavorite} currentFavorite={currentFavorite}>
       <CoinHeaderGridStyled>
         <div> {sym} </div>
         <ChangePercent data={data}/>
@@ -59,9 +65,9 @@ function PriceTile({sym, data}){
   );
 }
 
-function PriceTileCompact({sym, data}){
+function PriceTileCompact({sym, data, currentFavorite, setCurrentFavorite}){
   return (
-    <PriceTileStyled compact>
+    <PriceTileStyled onClick={setCurrentFavorite} compact currentFavorite={currentFavorite}>
       <JustifyLeft> {sym} </JustifyLeft>
       <ChangePercent data={data}/>
       <div>
@@ -76,7 +82,16 @@ export default function({price, index}){
   let data = price[sym]['USD'];
   let TileClass = index < 5 ? PriceTile : PriceTileCompact;
   return (
-    <TileClass sym={sym} data={data}> 
-    </TileClass>
+    <AppContext.Consumer>
+      {({currentFavorite, setCurrentFavorite}) => 
+        <TileClass 
+          sym={sym} 
+          data={data} 
+          currentFavorite={currentFavorite === sym}
+          setCurrentFavorite={() => setCurrentFavorite(sym)}
+        > 
+        </TileClass>
+      }
+    </AppContext.Consumer>
   )
 }
